83660e4de08189615f3f2286ab429fd2f836fa66
Bharath Bhushan <bharath.exe@gmail.com>
2014-04-16 00:22:30 -0400

    switched from sets to lists for sake of mutability

--- synonyms.py
+++ synonyms.py
@@ -1,19 +1,37 @@
 class Synonyms(dict):
 	def equate(self, firstPhrase, *phrases):
-		phraseSets = map(self.list, phrases)
-		self[firstPhrase] = self.list(firstPhrase).union(*phraseSets)
-		for phrase in self[firstPhrase]:
-			self[phrase] = self[firstPhrase]
+		firstPhraseList = self.list(firstPhrase)
+		phraseLists = map(self.list, phrases)
+		# Merge all phrases into first phrase's list
+		for list in phraseLists:
+			for phrase in list:
+				if phrase not in firstPhraseList:
+					firstPhraseList.append(phrase)
+		# Point all phrases to the updated list
+		for phrase in firstPhraseList:
+			self[phrase] = firstPhraseList
 			
 	def list(self, phrase):
-		if phrase in self:
-			return self[phrase]
-		else:
-			# Look for occurence in existing set
-			for key in self:
-				if phrase in self[key]:
-					self[phrase] = self[key]
-					return self[phrase]
-			# No set found => Make one
-			self[phrase] = {phrase}
-			return self[phrase]
+		# Make a new list if new phrase
+		if phrase not in self:
+			self[phrase] = [phrase]
+		# Prepare list response	
+		listToReturn = self[phrase]
+		listOfListsWithPhrase = [listToReturn]
+		# If the search comes back unexpectedly
+		# everything will need to be standardized
+		fuckeryAfoot = False
+		# Look for occurence in existing list
+		for key in self:
+			if phrase in self[key]:
+				if self[key] not in listOfListsWithPhrase:
+					# If another list has this phrase in it...
+					fuckeryAfoot = True
+					listOfListsWithPhrase.append(self[key])
+					for synonym in self[key]:
+						if synonym not in listToReturn:
+							listToReturn.append(synonym)
+		if fuckeryAfoot:
+			for phrase in listToReturn:
+				self[phrase] = listToReturn
+		return listToReturn
