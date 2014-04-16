b165e89402ea366ab1f9794051c001bd1552f6a1
Bharath Bhushan <bharath.exe@gmail.com>
2014-04-16 17:05:32 -0400

    added barebones implementation of Concept class

--- bottlenose.py
+++ bottlenose.py
@@ -1,7 +1,16 @@
 #!usr/bin/python2.7 -tt
 
+import sys
+sys.dont_write_bytecode = True
+# Keeps directory clean by not compiling local files to bytecode
+import grammar
+import utilities
+import synonyms
+import taxonomy
+import concepts
+
 def main():
-	print 'Hello World'
+  print 'Hello World'
   
 if __name__ == '__main__':
-	main()
+  main()
--- concepts.py
+++ concepts.py
@@ -1,31 +1,61 @@
+import re
+import networkx
+
+import sys
+sys.dont_write_bytecode = True
+# Keeps directory clean by not compiling utilities to bytecode
+
+import utilities
+
 class Concept(object):
-  def __init__(self, name, graph=None):
-    self.components = set()
-    self.actions = set()
+  graph = networkx.MultiDiGraph()
+  
+  def __init__(self, name):
+    self.name = utilities.camelCase(name)
+    self.potential = dict()
+    self.potential['components'] = set()
+    self.potential['actions'] = set()
+    self.potential['states'] = set()
+    self.undirected = dict()
+    self.undirected['components'] = set()
+    self.undirected['actions'] = set()
     self.states = set()
-    self.clauses = set()
-    self.graphs = [graph] if graph else []
+    Concept.graph.add_node(self)
   
-  def addComponent(component, target=None):
-    self.components.add(component)
+  def component(self, component, target=None, negative=False):
+    if re.match('^!', component): return self.component(component[1:], target, True)
+    component = utilities.camelCase(component)
+    self.potential['components'].add(component)
+    if not negative:
+      if not target: self.undirected['components'].add(component)
+      if isinstance(target, Concept):
+        Concept.graph.add_edge(self, target, key='has ' + component)
+    else:
+      if not target: self.undirected['components'].remove(component)
+      if isinstance(target, Concept):
+        Concept.graph.remove_edge(self, target, key='has ' + component)      
   
-  def addAction(action, target=None):
-    self.actions.add(action)
+  def action(self, action, target=None, negative=False):
+    if re.match('^!', action): return self.action(action[1:], target, True)
+    action = utilities.camelCase(action)
+    self.potential['actions'].add(action)
+    if not negative:
+      if not target: self.undirected['actions'].add(action)
+      if isinstance(target, Concept):
+        Concept.graph.add_edge(self, target, key=action)
+    else:
+      if not target: self.undirected['actions'].remove(action)
+      if isinstance(target, Concept):
+        Concept.graph.remove_edge(self, target, key=action)
+        
+  def state(self, state, negative=False):
+    if re.match('^!', state): return self.state(state[1:], True)
+    state = utilities.camelCase(state)
+    if not negative:
+      self.potential['states'].add(state)
+      if state not in self.states: self.states.add(state)
+    else:
+      if state in self.states: self.states.remove(state)
   
-  def addState(state):
-    self.states.add(state)
-  
-  def addClause(clause):
+  def clause(self, clause):
     self.clauses.add(clause)
-  
-  def components():
-    return self.components
-    
-  def actions():
-    return self.actions
-  
-  def states():
-    return self.states
-  
-  def clauses():
-    return self.clauses
