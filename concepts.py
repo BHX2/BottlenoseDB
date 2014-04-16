d1ecf5ed62e0336c8dc443d713302ca176e030b2
Bharath Bhushan <bharath.exe@gmail.com>
2014-04-16 14:01:15 -0400

    switched back to using mutable sets

---
+++ concepts.py
@@ -0,0 +1,31 @@
+class Concept(object):
+	def __init__(self, name, graph=None):
+		self.components = set()
+		self.actions = set()
+		self.states = set()
+		self.clauses = set()
+		self.graphs = [graph] if graph else []
+	
+	def addComponent(component, target=None):
+		self.components.add(component)
+	
+	def addAction(action, target=None):
+		self.actions.add(action)
+	
+	def addState(state):
+		self.states.add(state)
+	
+	def addClause(clause):
+		self.clauses.add(clause)
+	
+	def components():
+		return self.components
+		
+	def actions():
+		return self.actions
+	
+	def states():
+		return self.states
+	
+	def clauses():
+		return self.clauses
--- synonyms.py
+++ synonyms.py
@@ -5,41 +5,24 @@ sys.dont_write_bytecode = True
 import utilities
 
 class Synonyms(dict):
-	def equate(self, firstPhrase, *phrases):
-		firstPhrase = utilities.camelCase(firstPhrase)
+	def equate(self, *phrases):
 		phrases = map(utilities.camelCase, phrases)
-		firstPhraseList = self.list(firstPhrase)
-		phraseLists = map(self.list, phrases)
-		# Merge all phrases into first phrase's list
-		for list in phraseLists:
-			for phrase in list:
-				if phrase not in firstPhraseList:
-					firstPhraseList.append(phrase)
-		# Point all phrases to the updated list
-		for phrase in firstPhraseList:
-			self[phrase] = firstPhraseList
+		phraseSets = map(self.list, phrases)
+		mergedSet = set.union(*phraseSets)
+		for phrase in mergedSet:
+			self[phrase] = mergedSet
 			
 	def list(self, phrase):
 		phrase = utilities.camelCase(phrase)
-		# Make a new list if new phrase
-		if phrase not in self:
-			self[phrase] = [phrase]
-		# Prepare list response	
-		listToReturn = self[phrase]
-		listOfListsWithPhrase = [listToReturn]
-		# If the search comes back unexpectedly everything will need to be standardized
-		fuckeryAfoot = False
-		# Look for occurence in existing list
+		listOfSetsWithPhrase = []
 		for key in self:
 			if phrase in self[key]:
-				if self[key] not in listOfListsWithPhrase:
-					# If another list has this phrase in it...
-					fuckeryAfoot = True
-					listOfListsWithPhrase.append(self[key])
-					for synonym in self[key]:
-						if synonym not in listToReturn:
-							listToReturn.append(synonym)
-		if fuckeryAfoot:
-			for phrase in listToReturn:
-				self[phrase] = listToReturn
-		return listToReturn
+				if self[key] not in listOfSetsWithPhrase:
+					listOfSetsWithPhrase.append(self[key])
+		if len(listOfSetsWithPhrase) == 0:
+			self[phrase] = {phrase}
+		elif len(listOfSetsWithPhrase) > 1 or phrase not in self:
+			mergedSet = set.union(*listOfSetsWithPhrase)
+			for element in mergedSet:
+				self[element] = mergedSet
+		return self[phrase]
