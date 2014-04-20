9dd94be6f54904fddcb0c14bf82fe2ce4320457d
Bharath Bhushan <bharath.exe@gmail.com>
2014-04-20 13:36:28 -0400

    wrote unCamelCase function

--- utilities.py
+++ utilities.py
@@ -4,9 +4,24 @@ import string
 def camelCase(text):
   isNegative = re.match('^!', text)
   words = text.replace("'s", "s").translate(string.maketrans(string.punctuation, ' '*len(string.punctuation))).split()
-  response = words.pop(0);
+  response = words.pop(0)
   if isNegative:
     response = '!' + response
   for word in words:
     response += word.capitalize()
   return response
+  
+def unCamelCase(text):
+  text = re.sub("([A-Z0-9]*)([A-Z][a-z0-9]+)","\g<1> \g<2>", text)
+  text = re.sub("([a-z0-9])([A-Z])","\g<1> \g<2>", text).strip()
+  if text.istitle():
+    return text
+  else:
+    words = text.split()
+    response = words.pop(0)
+    for word in words:
+      if word.isupper():
+        response += (' ' + word)
+      else:
+        response += (' ' + word.lower())
+    return response
