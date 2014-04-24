b5946f65cf1d56c416505e3ece034f84ad7c7fcf
Bharath Bhushan <bharath.exe@gmail.com>
2014-04-24 15:33:08 -0400

    added more Context methods

--- contexts.py
+++ contexts.py
@@ -23,7 +23,7 @@ class Context:
     self._concepts['noun_phrases'].add(concept)
   
   def newVerbPhrase(self, verbPhrase):
-    concept = VerbPhrase(nounPhrase)
+    concept = VerbPhrase(verbPhrase)
     self.actionGraph.add_node(concept)
     self.stateGraph.add_node(concept)
     self._concepts['verb_phrases'].add(concept)
@@ -47,6 +47,31 @@ class Context:
         self._concepts['verb_phrases'].remove(act)
         self.stateGraph.remove_node(act)
         self.actionGraph.remove_node(act)
+  
+  def setComponent(self, parent, label, child=None):
+    if child:
+      self.componentGraph.add_edge(parent, child, label=label)
+    else:
+      child = self.newNounPhrase(label)
+      self.componentGraph.add_edge(parent, child, label=label)
+      
+  def unsetComponent(self, parent, child):
+    self.componentGraph.remove_edge(parent, child)
+  
+  def setState(self, subject, descriptor):
+    state = self.newDescriptor(descriptor)
+    self.stateGraph.add_edge(subject, state)
+
+  def unsetState(self, subject, state):
+    state.stateGraph.remove_edge(subject, state)
+    self._concepts['descriptors'].remove(state)
+    self.stateGraph.remove_node(state)
+    
+  def mergeConcepts(self, concept1, concept2):
+    #TODO: write this merge method
+    if isinstance(concept1, NounPhrase) and isinstance(concept2, NounPhrase):
+    elif isinstance(concept1, VerbPhrase) and isinstance(concept2, VerbPhrase):
+    elif isinstance(concept1, Descriptor) and isinstance(concept2, Descriptor):
   
   def queryNounPhrases(self, type):
     response = list()
